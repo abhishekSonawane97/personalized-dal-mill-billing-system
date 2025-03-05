8b3c7ca4b10a626f815d687c486c607a4867e0ea
Abhishek <abhisheksonawane1997@gmail.com>
2025-03-05 23:49:21 +0530

    added styles to multi language selector

--- client/src/pages/sidebar/Sidebar.jsx
+++ client/src/pages/sidebar/Sidebar.jsx
@@ -13,7 +13,6 @@ const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const sidebarToggle = () => {
-        // console.log(isOpen, 'sidebarToggle');
         setIsOpen(!isOpen);
     };
 
@@ -32,25 +31,21 @@ const Sidebar = () => {
 
     return (
         <div className={`flex h-20 bg-gradient-to-r from-indigo-800 to-blue-900 items-center ${isOpen ? '' : '' }`}>
-            {/* <aside className={`bg-white dark:bg-gray-800 w-full min-h-screen flex flex-col transition-all absolute left-0 duration-300 ease-in-out transform ${isOpen ? '' : '-translate-x-full z-10'}`}> */}
             <aside className={`bg-white w-full absolute dark:bg-gray-800 min-h-screen flex flex-col transition-all z-10 left-0 top-0 duration-300 ease-in-out transform ${isOpen ? 'min-h-screen' : '-translate-x-full z-10'} md:w-1/2 w-full lg:w-1/3`}>
                 <div className="p-4 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
                 <h1 className="text-2xl font-bold text-indigo-800  dark:text-white mx-4">Sai Dal Mill</h1>
                     <button onClick={()=>handleLink()} className="text-gray-500 hover:text-indigo-800 dark:text-gray-400 dark:hover:text-white font-bold text-2xl focus:outline-none">X
-                        {/* <CloseIcon className="h-6 w-6" /> */}
                     </button>
                 </div>
                 <nav className="flex-1 overflow-y-auto">
                     <ul className="p-4 space-y-2">
                         <li className="menu-item-hover animate-fade-in" style={{ animationDelay: '0.1s' }}>
                             <Link to={user? '/' : '/login'} className="flex items-center p-2 text-gray-700 dark:text-gray-200 rounded-lg hover:bg-indigo-100 dark:hover:bg-indigo-900 transition-colors duration-300" onClick={handleLink} >
-                                    {/* <HomeIcon className="h-6 w-6 mr-3" /> */}
                                     Home
                             </Link>
                         </li>
                         <li className="menu-item-hover animate-fade-in" style={{ animationDelay: '0.4s' }}>
                             <Link to={ user? "/search-receipt" : '/login' }className="flex items-center p-2 text-gray-700 dark:text-gray-200 rounded-lg hover:bg-indigo-100 dark:hover:bg-indigo-900 transition-colors duration-300" onClick={handleLink} >
-                                {/* <SettingsIcon className="h-6 w-6 mr-3" /> */}
                                 Search Receipt
                             </Link>
                         </li>
@@ -106,7 +101,7 @@ const Sidebar = () => {
             <button onClick={sidebarToggle} className="text-white hover:text-indigo-200 focus:outline-none mb-4 text-6xl flex items-center">
                 <h1 className="text-2xl font-bold text-white dark:text-white mx-4">Sai Dal Mill ☰ </h1>
                 </button>
-            <div className="p-4">
+            <div className="px-4 mb-6">
                 {
                     user? (
                         <Link to="/" className="flex items-center text-white dark:text-white hover:text-white-800 dark:hover:text-white transition-colors duration-300" onClick={()=>handleLink()}>
